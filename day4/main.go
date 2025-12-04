d4ff82f487e4009d1e44227e8e56b5bc2e8f049d
Georg Konrad <georg@wesolutions.at>
2025-12-04 10:56:50 +0100

    implemented part2

--- day4/main.go
+++ day4/main.go
@@ -23,6 +23,51 @@ func part1(grid [][]rune) int {
 	return hits
 }
 
+func part2Step(grid [][]rune) (int, [][]rune) {
+	marked := make([][]rune, len(grid))
+	for i := range grid {
+		marked[i] = make([]rune, len(grid[i]))
+		copy(marked[i], grid[i])
+	}
+
+	hits := 0
+
+	for y := 0; y < len(grid); y++ {
+		for x := 0; x < len(grid[y]); x++ {
+			if grid[y][x] != '@' {
+				continue
+			}
+			neigh := countNeighbourScrolls(grid, y, x)
+			if neigh <= 3 {
+				hits++
+				marked[y][x] = 'x'
+			}
+		}
+	}
+
+	return hits, marked
+}
+
+func part2(grid [][]rune) (int, [][]rune) {
+	hits, marked := part2Step(grid)
+
+	if hits == 0 {
+		return 0, grid
+	}
+
+	for y := range marked {
+		for x := range marked[y] {
+			if marked[y][x] == 'x' {
+				marked[y][x] = '.'
+			}
+		}
+	}
+
+	restHits, finalGrid := part2(marked)
+
+	return hits + restHits, finalGrid
+}
+
 func countNeighbourScrolls(grid [][]rune, y, x int) int {
 	h := len(grid)
 	w := len(grid[0])
@@ -51,6 +96,13 @@ func countNeighbourScrolls(grid [][]rune, y, x int) int {
 }
 
 func main() {
+	if len(os.Args) < 2 {
+		fmt.Fprintln(os.Stderr, "usage: go run main.go <1|2> < input.txt>")
+		os.Exit(1)
+	}
+
+	mode := os.Args[1]
+
 	scanner := bufio.NewScanner(os.Stdin)
 
 	var grid [][]rune
@@ -58,7 +110,7 @@ func main() {
 	for scanner.Scan() {
 		line := scanner.Text()
 		if len(line) == 0 {
-			continue // skip empty lines
+			continue
 		}
 		row := []rune(line)
 		grid = append(grid, row)
@@ -69,5 +121,17 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Printf("result: %d", part1(grid))
+	switch mode {
+	case "1":
+		res := part1(grid)
+		fmt.Printf("result: %d\n", res)
+
+	case "2":
+		res, _ := part2(grid)
+		fmt.Printf("result: %d\n", res)
+
+	default:
+		fmt.Fprintf(os.Stderr, "unknown mode: %s (use 1 or 2)\n", mode)
+		os.Exit(1)
+	}
 }
